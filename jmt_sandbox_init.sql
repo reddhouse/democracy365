4fe50073452625706a89693635f00f447fd5a060
JMT <reddhouse@gmail.com>
2020-10-09 21:05:47 -0700

    Move user creation into procedure

--- jmt_sandbox_init.sql
+++ jmt_sandbox_init.sql
@@ -130,6 +130,64 @@ $func$;
 -- * Procedures
 -- *
 
+create or replace procedure sandbox.insert_lorem_text ()
+language plpgsql
+as $proc$
+begin
+  with w (
+    word
+    -- Temporary table 'w', with 'word' column.
+) as (
+    -- Fill word column with lorem ipsum.
+    select
+      regexp_split_to_table('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '[\s., ]')
+    union
+    select
+      regexp_split_to_table('Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur.', '[\s., ]')
+    union
+    select
+      regexp_split_to_table('At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.', '[\s., ]')
+)
+-- Copy data from temporary table to permanent one, with some conditions.
+insert into sandbox.lorem (word)
+select
+  lower(word)
+from
+  w
+where
+  word is not null
+  and word <> '';
+end;
+$proc$;
+
+create or replace procedure sandbox.insert_new_user (email_address text)
+language plpgsql
+as $proc$
+begin
+  insert into sandbox.users (email_address, is_verified, account_updates)
+    values (email_address, 'false', 0);
+end;
+$proc$;
+
+create or replace procedure sandbox.insert_dummy_users (num_users int)
+language plpgsql
+as $proc$
+begin
+  insert into sandbox.users (email_address, verification_code_requested_on, verification_code_expiration, verification_code, is_verified, account_updates)
+  select
+    concat(sandbox.make_lorem (1), '@', sandbox.make_lorem (1), '.com') as email_address,
+    sandbox.random(now() - interval '3 months', now()) as verification_code_requested_on,
+    sandbox.random(now(), now() + interval '12 months') as verification_code_expiration,
+    substring(MD5(random()::text)
+    from 1 for 8) as verification_code,
+    not (sandbox.random(0, 1) = 0) as is_verified,
+    sandbox.random(0, 1) as account_updates
+  from
+  -- Optional table alias t(x) could theoretically refer to t.x in body of "loop" above if needed.
+  generate_series(1, num_users) as t (x);
+end;
+$proc$;
+
 create or replace procedure sandbox.insert_problem (problem_title text, problem_description text, problem_tags text[])
 language plpgsql
 as $proc$
@@ -189,7 +247,7 @@ begin
 end;
 $proc$;
 
-create or replace procedure sandbox.insert_dummy_solutions (max_solutions int)
+create or replace procedure sandbox.insert_dummy_solutions (max_solutions_per_problem int)
 language plpgsql
 as $proc$
 declare
@@ -210,7 +268,7 @@ begin
     sandbox.problems loop
       -- Prevent solutions from existing before the problem was created.
       age_limit_ts := p.date_created;
-      for counter in 1..sandbox.random(1, max_solutions)
+      for counter in 1..sandbox.random(1, max_solutions_per_problem)
       loop
         random_ts := sandbox.random(age_limit_ts, now());
         -- Make the next solution newer than the one just entered.
@@ -294,68 +352,34 @@ end;
 $proc$;
 
 -- *
--- * Data
+-- * Sandbox
 -- *
 
-with w (
-  word
-  -- Temporary table 'w', with 'word' column.
-) as (
-  -- Fill word column with lorem ipsum.
-  select
-    regexp_split_to_table('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '[\s., ]')
-  union
-  select
-    regexp_split_to_table('Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur.', '[\s., ]')
-  union
-  select
-    regexp_split_to_table('At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.', '[\s., ]')
-)
--- Copy data from temporary table to permanent one, with some conditions.
-insert into sandbox.lorem (word)
-select
-  lower(word)
-from
-  w
-where
-  word is not null
-  and word <> '';
+call sandbox.insert_lorem_text ();
 
-insert into sandbox.users (email_address, verification_code_requested_on, verification_code_expiration, verification_code, is_verified, account_updates)
-select
-  concat(sandbox.make_lorem (1), '@', sandbox.make_lorem (1), '.com') as email_address,
-  sandbox.random(now() - interval '3 months', now()) as verification_code_requested_on,
-  sandbox.random(now(), now() + interval '12 months') as verification_code_expiration,
-  substring(MD5(random()::text)
-  from 1 for 8) as verification_code,
-  not (sandbox.random(0, 1) = 0) as is_verified,
-  sandbox.random(0, 1) as account_updates
-from
--- Optional table alias t(x) could theoretically refer to t.x in body of "loop" above if needed.
-generate_series(1, 5) as t (x);
+-- Users
+call sandbox.insert_dummy_users (5);
 
--- *
--- * Output
--- *
+call sandbox.insert_new_user ('somebody@email.com');
 
+-- Problems
 call sandbox.insert_dummy_problems (4);
 
--- Non-dummy call to insert single problem into problems table.
 call sandbox.insert_problem ('This is a problem title', 'This is a description of a problem with no length limit?', '{"single", "word", "tags", "go", "here with spaces?"}');
 
-call sandbox.insert_dummy_solutions (3);
+-- Solutions
+call sandbox.insert_dummy_solutions (max_solutions_per_problem := 3);
 
--- Non-dummy call to insert single solution into solutions table.
 call sandbox.insert_solution (problem_id := 1, solution_title := 'This is a solution title', solution_description := 'This is a description of a solution with no length limit?', solution_tags := '{"single", "word", "tags", "go", "here with spaces?"}');
 
+-- Links
 call sandbox.insert_dummy_links ();
 
--- Non-dummy call to insert single problem link into links table.
 call sandbox.insert_problem_link (problem_id := 1, link_title := 'great article with more info on foobar', link_url := 'https://google.com');
 
--- Non-dummy call to insert single solution link into links table.
 call sandbox.insert_solution_link (solution_id := 1, link_title := 'great article with more info on barfoo', link_url := 'https://google.com');
 
+-- Display
 select
   *
 from
@@ -376,5 +400,6 @@ select
 from
   sandbox.links;
 
+-- Commit or Rollback
 rollback;
 
